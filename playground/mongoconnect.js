c7d50e378c60f77dcc80efad1cd7127119119da6
thenullterminator <dazz2803@gmail.com>
2019-06-03 12:22:10 +0530

    Basics of MongoDB

---
+++ playground/mongoconnect.js
@@ -0,0 +1,13 @@
+const MongoClient=require("mongodb").MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp',(err,db)=>{
+
+    if(err)
+    {
+        return console.log("Unable to connect to MongoDB server :( ");
+    }
+
+    console.log("Connected to MongoDB server :) ");
+
+    db.close();
+});
