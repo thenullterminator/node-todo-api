5638907a95a33c24bf0341f9932280470334eb9a
thenullterminator <dazz2803@gmail.com>
2019-06-05 14:59:29 +0530

    adding some comments

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
-const {mongoose}=require('./database/mongooseconfig');
+// local user defined imports
+const {mongoose}=require('./database/mongooseconfig');//connected to mongoDB
+// Our mongoose models
 const {ToDoModel}=require('./models/todo');
 const {UserModel}=require('./models/user');
 
+// Third party imports 
 const express=require('express');
 const bodyparser=require('body-parser');
+// Setting up the port
 const port=process.env.PORT||3000;
 
+// creating a brand new app
 var app=express();
 
+// Setting up the middleware to recieve body in the form of json
 app.use(bodyparser.json());
 
+// post route to create todos
 app.post('/todos',(req,res)=>{
 
     var newtodo=new ToDoModel(req.body);
@@ -21,6 +28,7 @@ app.post('/todos',(req,res)=>{
     })
 });
 
+// get route to list all existing todos
 app.get('/todos',(req,res)=>{
 
 ToDoModel.find().then((docs)=>{
@@ -28,10 +36,11 @@ ToDoModel.find().then((docs)=>{
 },(e)=>{
     res.status(401).send(e);
 })
-
 });
 
 
+
+// setting up listener
 app.listen(port,()=>{
 console.log(`Server is up on port ${port}`);
 });
